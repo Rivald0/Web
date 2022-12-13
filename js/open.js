a0bedc706b0f80688d8f3a4165a443dd801289af
Rivald0 <85506124+Rivald0@users.noreply.github.com>
2022-12-13 22:37:30 +0700

    Add files via upload

---
+++ js/open.js
@@ -0,0 +1,19 @@
+function home() {
+    alert("Selamat datang di beranda");
+};
+
+function info() {
+    alert("Selamat datang di Informasi Sekolah");
+};
+
+function alamat() {
+    alert("Selamat datang di Kontak");
+};
+
+function help() {
+    alert("Fitur ini segera datang");
+};
+
+function contact() {
+    alert("Fitur ini segera datang");
+};
